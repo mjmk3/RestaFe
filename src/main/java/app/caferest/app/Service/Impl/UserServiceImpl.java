fdccba151b250074cbc02ae35ba786dc2e6d1b16
Mohamad J. Makki <malanbaki@transforat.com>
2022-12-19 15:05:35 +0300

    updating the service implementation for the user

--- src/main/java/app/caferest/app/Service/Impl/UserServiceImpl.java
+++ src/main/java/app/caferest/app/Service/Impl/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package app.caferest.app.Service.Impl;
 
+import app.caferest.app.DAO.UserRepo;
+import app.caferest.app.Domain.User;
 import app.caferest.app.Service.UserService;
 import app.caferest.app.Utility.CafeUtils;
 import app.caferest.app.Utility.Constant.CafeConstant;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,11 +16,19 @@ import java.util.Map;
 @Service
 @Slf4j // for development purpose
 public class UserServiceImpl implements UserService {
+
+    UserRepo userRepo;
+
+    @Autowired
+    public UserServiceImpl(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
     @Override
     public ResponseEntity<String> signUp(Map<String, String> requestMap) {
         log.info("Sign-Up Form {}", requestMap);
         if(validateSignUpMap(requestMap)) {
-
+            User user =
         } else {
             return CafeUtils.getResponseEntity(CafeConstant.INVALID_DATA, HttpStatus.BAD_REQUEST);
         }
